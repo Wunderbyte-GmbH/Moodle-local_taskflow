05dfd2c9c04e7e2299eeb3a4ae9c9517a0c177df
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2025-07-23 14:33:30 +0200

    Linting: linting code

--- classes/local/assignment_process/assignment_preprocessor.php
+++ classes/local/assignment_process/assignment_preprocessor.php
@@ -160,7 +160,7 @@ class assignment_preprocessor {
 
     /**
      * React on the triggered event.
-     * @param string @userid
+     * @param string $userid
      * @return void
      */
     public function set_this_user($userid): void {
