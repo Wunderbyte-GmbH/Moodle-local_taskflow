310b7df1f963111f5acab25aaeb5c2066453109f
David Ala <davidala@Mac.dhw>
2025-08-05 11:29:12 +0200

    Bugfix: cohort core function gives back more than 25 cohorts

--- classes/local/units/organisational_units/cohorts.php
+++ classes/local/units/organisational_units/cohorts.php
@@ -55,7 +55,7 @@ class cohorts implements organisational_units_interface {
         global $DB;
         $this->cohortsrelation = $DB->get_records(self::TABLENAME, ['active' => 1], '', 'childid, parentid');
         $context = context_system::instance();
-        $cohortsdata = cohort_get_cohorts($context->id);
+        $cohortsdata = cohort_get_cohorts($context->id, 0, 0);
         $this->cohorts = $cohortsdata['cohorts'];
     }
 
