d007471c418d2ee531c53114d0b9ef01c694dcae
Georg Maißer <georg.maisser@wunderbyte.at>
2025-10-20 11:23:21 +0200

    Version 0.1.1 (2025102000)

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_taskflow';
-$plugin->release = '0.1.0';
+$plugin->release = '0.1.1';
 $plugin->supported = [403, 500];
-$plugin->version = 2025101701;
+$plugin->version = 2025102000;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
