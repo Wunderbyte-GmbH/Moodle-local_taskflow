77e59e23c131f721af1926c9f3ed43817637c493
Georg Maißer <georg.maisser@wunderbyte.at>
2025-11-05 13:02:45 +0100

    Tests: Add correct settings for ksw tests

--- tests/generator/lib.php
+++ tests/generator/lib.php
@@ -261,22 +261,26 @@ class local_taskflow_generator extends testing_module_generator {
                 ];
                 break;
             case 'ksw':
-                                $taskflowadaptersettings = [
+                $taskflowadaptersettings = [
                     taskflowadapter::TRANSLATOR_USER_FIRSTNAME => "Firstname",
                     taskflowadapter::TRANSLATOR_USER_LASTNAME => "LastName",
                     taskflowadapter::TRANSLATOR_USER_EMAIL => "DefaultEmailAddress",
                     taskflowadapter::TRANSLATOR_USER_ORGUNIT => "Organisation",
+                    taskflowadapter::TRANSLATOR_USER_EXTERNALID => "userID",
+                    taskflowadapter::TRANSLATOR_USER_CONTRACTEND => "ExitDate",
+                    taskflowadapter::TRANSLATOR_USER_CONTRACTSTART => "EntryDate",
+                    taskflowadapter::TRANSLATOR_USER_SUPERVISOR_EXTERNAL => 'Manager_Id',
+                    taskflowadapter::TRANSLATOR_USER_SUPERVISOR => 'supervisor',
+                    "supervisor_external" => taskflowadapter::TRANSLATOR_USER_SUPERVISOR_EXTERNAL,
                     "orgunit" => taskflowadapter::TRANSLATOR_USER_ORGUNIT,
-                    taskflowadapter::TRANSLATOR_USER_SUPERVISOR => "Manager_Email",
                     "supervisor" => taskflowadapter::TRANSLATOR_USER_SUPERVISOR,
                     "externalid" => taskflowadapter::TRANSLATOR_USER_EXTERNALID,
-                    taskflowadapter::TRANSLATOR_USER_CONTRACTEND => "ExitDate",
                     "contractend" => taskflowadapter::TRANSLATOR_USER_CONTRACTEND,
                     "contractstart" => taskflowadapter::TRANSLATOR_USER_CONTRACTSTART,
                     'organisational_unit_option' => 'cohort',
                     'user_profile_option' => 'thour',
                     'supervisor_field' => 'supervisor',
-                                ];
+                ];
                 break;
             case 'standard':
             default:
