eb5a24528804f2e468ee4975aaa847b4a435ffc2
David Ala <davidala@Mac.dhw>
2025-11-06 11:28:54 +0100

    Version(2025110600)

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_taskflow';
 $plugin->release = '0.1.1';
 $plugin->supported = [403, 500];
-$plugin->version = 2025102900;
+$plugin->version = 2025110600;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
