22b609272b4f2d21bb25e72df2b15e65bfd0fae5
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2025-11-18 16:02:05 +0100

    Version (2025111800)

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_taskflow';
 $plugin->release = '0.2.2';
 $plugin->supported = [403, 500];
-$plugin->version = 2025111600;
+$plugin->version = 2025111800;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
