0b6e0a2b4f418a0e94d3f3eddda6ec31332400d1
Georg Maißer <georg.maisser@wunderbyte.at>
2025-12-22 14:38:39 +0100

    Bugfix: Fix localized placeholders Wunderbyte-GmbH/moodle-taskflowadapter_tuines#9

--- classes/local/messages/placeholders/placeholders_factory.php
+++ classes/local/messages/placeholders/placeholders_factory.php
@@ -61,7 +61,7 @@ class placeholders_factory {
     public static function has_placeholders($message) {
         $potentialplaceholders = [];
         foreach ($message as $part) {
-            preg_match_all('/<([a-zA-Z0-9_]+)>/', $part, $matches);
+            preg_match_all('/<([a-zA-Z0-9_]+)(?:\s+(?:de|en|fr))?>/', $part, $matches);
             $potentialplaceholders = array_merge($matches[1], $potentialplaceholders);
         }
         foreach ($potentialplaceholders as $potentialplaceholder) {
@@ -83,7 +83,7 @@ class placeholders_factory {
         $potentialplaceholders = [];
 
         foreach ($message as $part) {
-            preg_match_all('/<([a-zA-Z0-9_]+)>/', $part, $matches);
+            preg_match_all('/<([a-zA-Z0-9_]+)(?:\s+(?:de|en|fr))?>/', $part, $matches);
             $potentialplaceholders = array_merge($matches[1], $potentialplaceholders);
         }
         foreach ($potentialplaceholders as $potentialplaceholder) {
