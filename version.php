1f6634a27470aae6d3e430d50abda31f0091842d
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2026-02-09 15:45:54 +0100

    Version(2026020900)

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_taskflow';
 $plugin->release = '0.2.5';
 $plugin->supported = [405, 500];
-$plugin->version = 2026013000;
+$plugin->version = 2026020900;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
