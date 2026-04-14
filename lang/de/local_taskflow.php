aba067dba4412f10a219a4127fbf078bb94f13b2
David Ala <david.ala@wunderbyte.at>
2026-04-14 16:01:31 +0200

    Improvement: use first HR user as usermodified for history

--- lang/de/local_taskflow.php
+++ lang/de/local_taskflow.php
@@ -228,6 +228,7 @@ $string['learningpath'] = 'Lernpfad';
 $string['lessfunctions'] = '<div class="alert alert-danger" role="alert">Nicht alle Funktionen wurden beim letzten Speichern ausgewählt. Dies kann zu Fehlern führen.</div>';
 $string['linktomessages'] = '<a href="/local/taskflow/message_form/editmessage.php" target="_blank">Verwalte Nachrichten</a>';
 $string['longleave'] = 'Längerer Ausfall';
+$string['mailsend:messagename'] = 'Die Nachricht mit dem Namen {$a} wurde gesendet';
 $string['managemessages'] = 'Nachrichten verwaltens';
 $string['managemessagesdescription'] = 'Taskflow-Nachrichten verwalten';
 $string['manuallyprolonged'] = 'Manuel verlängert';
