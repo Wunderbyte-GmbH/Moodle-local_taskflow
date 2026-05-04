618bea407bbd005b7f6b958ec423df204801b114
David Ala <david.ala@wunderbyte.at>
2026-05-04 17:08:58 +0200

    Version (2026042702)

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_taskflow';
 $plugin->release = '0.2.5';
 $plugin->supported = [405, 502];
-$plugin->version = 2026031701;
+$plugin->version = 2026031702;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
