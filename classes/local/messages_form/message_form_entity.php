17f005176f2858c12aba6cd729e241035e47688f
David Ala <david.ala@wunderbyte.at>
2026-05-18 12:50:31 +0200

    Resolve Merge conflict

--- classes/local/messages_form/message_form_entity.php
+++ classes/local/messages_form/message_form_entity.php
@@ -41,6 +41,7 @@ class message_form_entity {
         global $USER, $DB;
         $record = new stdClass();
         $record->id = $formdata->id ?? 0;
+        $this->set_messagetype($formdata, $record);
         $record->message = json_encode([
             'heading' => $formdata->heading,
             'body' => $formdata->body['text'],
@@ -70,6 +71,25 @@ class message_form_entity {
             $DB->update_record('local_taskflow_messages', $record);
         }
         return $record->id;
+    }
+
+    /**
+     * Definition.
+     * @param stdClass $formdata
+     * @param stdClass $record
+     * @return void
+     */
+    private function set_messagetype($formdata, &$record) {
+        $record->class = $formdata->messagetypes ?? 'standard';
+        if (
+            $record->class == 'standard' &&
+            empty($formdata->sendstart)
+        ) {
+            $record->class = 'onevent';
+        } else if (str_contains($formdata->sendstart ?? '', 'onrequest')) {
+            $record->class = $formdata->sendstart;
+        }
+        return;
     }
 
     /**
