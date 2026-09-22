e904bc2a15b4f2840a75c5749b95a1735abe1a66
David Ala <david.ala@wunderbyte.at>
2026-09-22 08:08:12 +0200

    Improvement: bulkchecker actions in history Wunderbyte-GmbH/Wunderbyte-GmbH#2365

---
+++ classes/local/history/types/limit_reached.php
@@ -0,0 +1,63 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Limit reached type to manage output history.
+ *
+ * @package local_taskflow
+ * @copyright 2026 Wunderbyte GmbH
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_taskflow\local\history\types;
+
+use local_taskflow\taskflow_stringmanager;
+
+/**
+ * Limit reached type to manage output history.
+ */
+class limit_reached extends base {
+    /**
+     * Render the output: which message was blocked, and how big the burst was.
+     * @return string
+     */
+    public function render_additional_data(): string {
+        $messagename = $this->jsonobject->data ?? '';
+        if (is_object($messagename)) {
+            // For old entries, the message name was stored as an object with a heading property.
+            $messagename = $messagename->heading ?? '';
+        }
+        if (empty($messagename)) {
+            return '';
+        }
+        $count = (int) ($this->jsonobject->count ?? 0);
+        if (empty($count)) {
+            return taskflow_stringmanager::get_string('limitreached:messagenamenocount', $messagename);
+        }
+        return taskflow_stringmanager::get_string('limitreached:messagename', (object) [
+            'name' => $messagename,
+            'count' => $count,
+        ]);
+    }
+
+    /**
+     * Has additional data
+     * @return bool
+     */
+    public function has_additional_data(): bool {
+        return true;
+    }
+}
